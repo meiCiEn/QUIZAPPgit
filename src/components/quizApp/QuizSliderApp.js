302d3470f3e0aea5d30470531c0b97130f31dfac
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2023-03-19 14:48:41 +0100

    Updated Quiz Slider

--- src/components/quizApp/QuizSliderApp.js
+++ src/components/quizApp/QuizSliderApp.js
@@ -4,6 +4,7 @@ import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box'
 import { styled } from '@mui/material/styles';
 
+// document.getElementsByClassName(classNames)
 
 const QuizSlider = styled(Slider)({
 
@@ -13,8 +14,8 @@ const QuizSlider = styled(Slider)({
       border: 'none',
     },
     '& .MuiSlider-thumb': {
-      height: 24,
-      width: 24,
+      height: 30,
+      width: 30,
       backgroundColor: '#fff',
       border: '2px solid currentColor',
       '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
@@ -24,22 +25,24 @@ const QuizSlider = styled(Slider)({
         display: 'none',
       },
     },
+    '& .MuiSlider-markLabel': {
+    },
   
     '& .MuiSlider-valueLabel': {
       lineHeight: 1.2,
-      fontSize: 16,
+      fontSize: 14,
       background: 'unset',
-      padding: 0,
-      width: 50,
-      height: 50,
+      padding: 10,
+      width: 80,
+      height: 80,
       borderRadius: '50% 50% 50% 0',
-      backgroundColor: '#ff5722',
+      backgroundColor: ' #090088',
       transformOrigin: 'bottom left',
       transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
       '&:before': { display: 'none' },
       '&.MuiSlider-valueLabelOpen': {
         transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-        backgroundColor: '#ff8a65',
+        backgroundColor: '#090088',
       },
       '& > *': {
         transform: 'rotate(45deg)',
@@ -51,15 +54,27 @@ const QuizSlider = styled(Slider)({
   
 
 const QuizSliderApp = (props) => {
-  const { defaultValue, min, max, onChange, onChangeCommitted, disabled, questions } = props;
-console.log(questions);
+  const { defaultValue, min, max, onChange, onChangeCommitted, disabled, questions, getAriaValueText, unit, valueLabelFormat, item, step } = props;
+// console.log(questions);
+
+const marks = [
+  
+  {
+    value: min,
+    label: `${min} ${unit}`
+  },
+  {
+    value: max,
+    label: `${max} ${unit}`
+  }
+];
 
   return (
     <>
     
   <Box display="flex" flexDirection="column" m={10}>
     <QuizSlider 
-    valueLabelDisplay='auto'
+    valueLabelDisplay='on'
     defaultValue={defaultValue}
     min={min}
     max={max}
@@ -67,6 +82,10 @@ console.log(questions);
     onChangeCommitted={onChangeCommitted}
     disabled={disabled}
     questions={questions}
+    getAriaValueText={getAriaValueText}
+    marks={marks}
+    unit={unit}
+    valueLabelFormat={valueLabelFormat}
 
     />
     
