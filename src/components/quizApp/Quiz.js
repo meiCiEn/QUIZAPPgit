84fcbf2eb99e9eed3ab42a20d08c0d287800a75a
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2023-03-19 14:50:26 +0100

    Added styles to quiz

--- src/components/quizApp/Quiz.js
+++ src/components/quizApp/Quiz.js
@@ -1,88 +1,142 @@
-import React, { useState } from 'react';
+
+import React, { useEffect } from 'react';
 import QuizSliderApp from './QuizSliderApp';
+import BlurredBackground from './../layout/BlurredBackground.js';
 
 
 
 
 
-const Quiz = ( { showQuiz, question, questions, checkAnswer, answerText, setSlider, sliderMin, sliderMax, sliderDefault, questionIndex, nextQuestion, showAnswer, showAnswerText, gameOver, val } ) =>
+const Quiz = ( { showQuiz, question, questions, checkAnswer, answerAssessment, answerText, questionIndex, nextQuestion, showAnswer, answer, showAnswerText, gameOver, handleValueChange, getDefaultValue, value } ) =>
 {
-    const [ value, setValue ] = useState( 1 );
-    const handleValueChange = ( event ) =>
-    {
-        setValue( event.target.value )
-        setSlider( value );
-        checkAnswer();
 
-    };
-    const handleValueCommitted = (event) =>
+
+    function valueLabelFormat ( value, unit )
     {
-        console.log(value)
+
+        const hours = Math.floor( value / 60 );
+        const minutes = value % 60;
+
+        if ( unit === "minutes" )
+        {
+            if ( minutes === 0 && hours >= 1 )
+            {
+                return (
+                    `${ hours } h` );
+
+            } else if ( minutes === 0 && hours === 0 )
+            {
+                return (
+                    `0 min`
+                );
+
+            } else if ( minutes >= 0 && hours === 0 )
+            {
+                return (
+                    `${ minutes } min`
+                );
+
+
+            } else
+            {
+                return ( `${ hours } h ${ minutes } min` );
+
+            }
+
+        } else
+        {
+
+            return (
+                `${ value } ${ unit }`
+
+            );
+
+        }
+
+    }
+
+    function valuetext ( value, label )
+    {
+        return `${ value } ${ label }`;
     }
 
 
+
+
+    // sliderEndMarkLabel1.innerHTML = "3 heures";
+
+
+
     return (
+
         <section style={ { display: `${ showQuiz ? 'block' : 'none' }` } }>
-            <div className="container">
-                <div className="padding">
-                    <h5>{ question?.question }</h5>
-                    <h5>{ questions.indexOf( question ) + 1 } / { questions?.length }</h5>
-                    {/* <h1 className="padding">{ question.sliderMin }</h1> */ }
-                </div>
-                <div className="padding">
-                    <p>Déplacez le curseur pour deviner, puis vérifiez la réponse.</p>
-                </div>
-                {/* Map sliders into separate array and only show the slider that's relevant to the question */}
-                {questions.map((item, index) => (
-                <div key={index} className="padding" style={ { display: `${ index===questionIndex ? 'block' : 'none' }` } }>
-                    
-                    <QuizSliderApp
+            <BlurredBackground>
+                <div className="container text-center">
+                    <p >Question { questions.indexOf( question ) + 1 } / { questions?.length }</p>
+                    <div className="py-6">
+                        <p className="text-2xl font-medium">{ question?.question }</p>
 
-                        min={ item.sliderMin }
-                        max={ item.sliderMax }
-                        defaultValue={ item.sliderMin }
-                        onChange={ handleValueChange }
-                        onChangeCommitted={ handleValueCommitted }
-                        disabled={ showAnswer }
-                        
-                    />
+                        {/* <h1 className="">{ question.sliderMin }</h1> */ }
+                    </div>
+                    <div className="">
+                        <p>Déplacez le curseur pour deviner, puis vérifiez la réponse.</p>
+                    </div>
+                    {/* Map sliders into separate array and only show the slider that's relevant to the question */ }
+                    { questions.map( ( item, index ) => (
+                        <div id={ `slider-${ questionIndex }` } key={ index } className="pt-12" style={ { display: `${ index === questionIndex ? 'block' : 'none' }` } }>
 
-                    //TODO Disable slider when question answered
-                    {/* <p>{ value }</p> */ }
-                </div>
-                )
-                )
-            }
-                
+                            <QuizSliderApp
+
+                                min={ item.sliderMin }
+                                max={ item.sliderMax }
+                                defaultValue={ getDefaultValue( item.sliderMin ) }
+                                onChange={ handleValueChange }
+                                // onChangeCommitted={ handleValueCommitted(item.sliderMin) }
+                                disabled={ showAnswer }
+                                getAriaValueText={ valuetext }
+                                marks={ item.marks }
+                                unit={ item.unit }
+                                item={ item }
+                                valueLabelFormat={ valueLabelFormat( value, item.unit ) }
+
+                            //TODO convert minutes to hours and minutes
+                            />
+
+                        </div>
+                    )
+                    )
+                    }
 
 
-                {
-                    ( questionIndex + 1 ) !== questions.length ?
-                        <>
-                            <div className="padding">
-                                <button onClick={ showAnswerText } disabled={ !checkAnswer }>Afficher Réponse</button>
+
+                    {
+                        ( questionIndex + 1 ) !== questions.length ?
+                            <div className="flex gap-2 justify-center" >
+                                <div className="flex ">
+                                    <button className="inline-flex items-center justify-center px-8 py-4 font-semibold tracking-wide text-white bg-slate-900 hover:bg-slate-600 rounded-lg h-[60px] disabled:opacity-20" onClick={ showAnswerText } disabled={ !checkAnswer || !answer }>Afficher Réponse</button>
+                                </div>
+                                <div className="flex">
+                                    <button className="inline-flex items-center justify-center px-8 py-4 font-semibold tracking-wide text-white bg-slate-900 hover:bg-slate-600 rounded-lg h-[60px] disabled:opacity-20" onClick={ nextQuestion } disabled={ !showAnswer }>Question Suivante</button>
+                                </div>
+
                             </div>
-                            <div className="padding">
-                                <button onClick={ nextQuestion } disabled={ !showAnswer }>Question Suivante</button>
-                            </div>
+                            :
+                            <>
+                                <div className="">
+                                    <button className="inline-flex items-center justify-center px-8 py-4 font-semibold tracking-wide text-white bg-slate-900 hover:bg-slate-600 rounded-lg h-[60px] disabled:opacity-50" onClick={ showAnswerText } disabled={ !checkAnswer }>Afficher Réponse</button>
+                                </div>
+                                <div className="">
+                                    <button className="inline-flex items-center justify-center px-8 py-4 font-semibold tracking-wide text-white bg-slate-900 hover:bg-slate-600 rounded-lg h-[60px] disabled:opacity-50" onClick={ gameOver } disabled={ !showAnswer }>Show Result</button>
+                                </div>
+                            </>
+                    }
+                    <div className="" style={ { display: `${ showAnswer ? 'block' : 'none' }` } }>
+                        <p className="text-xl py-4 font-medium">{ answerAssessment }</p>
+                        <p>{ answerText }</p>
+                    </div>
 
-                        </>
-                        :
-                        <>
-                            <div className="padding">
-                                <button onClick={ showAnswerText } disabled={ !checkAnswer }>Afficher Réponse</button>
-                            </div>
-                            <div className="padding">
-                                <button onClick={ gameOver } disabled={ !showAnswer }>Show Result</button>
-                            </div>
-                        </>
-                }
-                <div className="padding" style={ { display: `${ showAnswer ? 'block' : 'none' }` } }>
-                    <p>{ answerText }</p>
                 </div>
-
-            </div>
-
+            </BlurredBackground>
         </section>
     );
 };
