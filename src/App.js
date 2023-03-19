ea91d1e6ac7bf65cddb6cea234d5f41bdc7b4ec5
meiCiEn <99802093+meiCiEn@users.noreply.github.com>
2023-03-19 14:55:58 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,42 +5,83 @@ import Quiz from './components/quizApp/Quiz';
 import Start from './components/quizApp/Start';
 import Result from './components/quizApp/Result';
 import { questions } from "./components/quizApp/questions";
+import Layout from './components/layout/Layout';
 import './App.css';
+
 
 export default function App ()
 {
-  // set states
+  // SET STATES
   const [ questionIndex, setQuestionIndex ] = useState( 0 ); // index of questions array of objects
-  const [ question, setQuestion ] = useState( {} );
-  const [ questionNext, setQuestionNext ] = useState( {} );
+  const [ question, setQuestion ] = useState( {} ); // the question being shown now
+  const [ questionNext, setQuestionNext ] = useState( {} ); // the question that follows
   const [ predictedAnswer, setPredictedAnswer ] = useState( 0 ); // the user's answer (the value of the slider)
-  const [ predictedAnswerCheck, setPredictedAnswerCheck ] = useState( false );
-  const [ showAnswer, setShowAnswer ] = useState( false );
-  const [ answerText, setAnswerText ] = useState( "" ); // the text the user receives depending on how close his/her answer is to the real answer
+  const [ predictedAnswerCheck, setPredictedAnswerCheck ] = useState( false ); // Has the user's answer been checked against the actual answer ?
+  const [ showAnswer, setShowAnswer ] = useState ( false ); // whether or not to reveal the answer
+  const [ answerAssessment, setAnswerAssessment ] = useState( "" ); // the feedback the user receives depending on how close his/her answer is to the real answer
+  const [ answerText, setAnswerText ] = useState( "" ); // the explanation the user receives depending on how close his/her answer is to the real answer
   const [ marks, setMarks ] = useState( 0 );
   const [ sliderMin, setSliderMin ] = useState( 1 ); // minimum value of slider scale
   const [ sliderMax, setSliderMax ] = useState( 100 ); // maximum value of slider scale
-  const [ sliderDefault, setSliderDefault ] = useState( 1 ); // 
+  const [ sliderDefault, setSliderDefault ] = useState( 1 ); // default Value of slider
+  const [ valueSet, setValueSet ] = useState (false);
 
   // display controlling states
   const [ showStart, setShowStart ] = useState( true );
   const [ showQuiz, setShowQuiz ] = useState( false );
   const [ showResult, setShowResult ] = useState( false );
-
-  console.log(questions);
-
   
+  // value of slider
+  const [ value, setValue ] = useState( 1 );
+  const [ answer, setAnswer ] = useState(false);
+
+  // 
+
+  const handleValueChange = (event  ) =>
+    {
+  
+        getSliderValue(0);
+        setValue( event.target.value );
+        getSliderValue( value );
+        checkAnswer();
+        setAnswer(true);
+  
+        console.log(`value = ${value}`)
+  
+  
+    };
+
   // Set a Single Question
   useEffect( () =>
   {
+  // code in useEffect runs after the component has rendered
     if ( questions.length > questionIndex )
     {
       setQuestion( questions[ questionIndex ] );
       setQuestionNext( questions[ questionIndex + 1 ] );
-    }
+      setValue(questions[questionIndex].sliderMin);
+      setValueSet( true );
+      // if sliderLabel1 exists (because it has to be rendered first), change its text to the following:
+      if (sliderLabel1) {sliderLabel1.innerHTML="3 heures";}
+
+    } 
   }, [ questions, questionIndex ] );
 
-  // Start Quiz
+
+  const sliderLabel= [...document.querySelectorAll(".css-yafthl-MuiSlider-markLabel")];
+
+
+  let sliderLabel1=sliderLabel[1];
+  // console.log(sliderLabel1);
+  
+
+
+  // if (questions[questionIndex] === 0) {
+    
+  // }
+  
+
+  // Start the quiz
   const startQuiz = () =>
   {
     setShowStart( false );
@@ -54,72 +95,86 @@ export default function App ()
     if ( question.correct.value === predictedAnswer )
     {
       // setScore( score + 1 );
+      setAnswerAssessment( question.correct.assessment );
       setAnswerText( question.correct.text );
 
     } else if ( predictedAnswer >= question.veryLow.min && predictedAnswer <= question.veryLow.max )
     {
+      setAnswerAssessment( question.veryLow.assessment );
       setAnswerText( question.veryLow.text );
     } else if ( predictedAnswer >= question.bitLow.min && predictedAnswer <= question.bitLow.max )
     {
+      setAnswerAssessment( question.bitLow.assessment );
       setAnswerText( question.bitLow.text );
     } else if ( predictedAnswer >= question.bitHigh.min && predictedAnswer <= question.bitHigh.max )
     {
+      setAnswerAssessment( question.bitHigh.assessment );
       setAnswerText( question.bitHigh.text );
     } else if ( predictedAnswer >= question.veryHigh.min && predictedAnswer <= question.veryHigh.max )
     {
+      setAnswerAssessment( question.veryHigh.assessment );
       setAnswerText( question.veryHigh.text );
     }
     // console.log(predictedAnswer);
-    // console.log(answerText);
+    // console.log(answerAssessment);
   };
-
-  //TODO find out how to recuperate value from slider and plug into checkAnswer
-
-  const showAnswer1 = () =>
-  {
-    console.log( predictedAnswer );
-    checkAnswer();
-    console.log( answerText );
-  };
-
-
 
   const showAnswerText = ( props ) =>
   {
     setShowAnswer( true );
+    setValueSet( false );
+    
   };
 
-  // Next Question
+  // Next Question - resets values, moves question index on
   const nextQuestion = () =>
   {
-    setQuestionIndex( questionIndex + 1 );
-    setAnswerText( '' );
-    setShowAnswer( false );
-    setPredictedAnswerCheck( false );
-    setPredictedAnswer( 0 );
+   
+    setPredictedAnswer( 0 ); // resets slider value
+    setQuestionIndex( questionIndex + 1 ); // increments question index by 1
+    setAnswerText( '' ); // resets the answer text
+    setShowAnswer( false ); // resets so answer is hidden
+    setAnswer(false); //
+    setPredictedAnswerCheck( false ); // don't check the answer yet
+    
 
-    console.log( `question.sliderMin = ${ question.sliderMin }` );
-    setSliderMin( questionNext.sliderMin );
-    setSliderMax( questionNext.sliderMax );
-    setSliderDefault( questionNext.sliderMin );
-    console.log( `sliderMin = ${ sliderMin }, sliderMax = ${ sliderMax }, sliderDefault = ${ sliderDefault }` );
+    // console.log( `question.sliderMin = ${ question.sliderMin }` );
+    setSliderDefault( questionNext.sliderMin ); // takes minimum from scale of next question
+    setSliderMin( questionNext.sliderMin ); // takes minimum from scale of next question
+    setSliderMax( questionNext.sliderMax ); // takes maximum from scale of next question
+
 
   };
 
-  const setSlider = ( val ) =>
+  const getSliderValue = ( val ) =>
   {
-    setPredictedAnswer( val );
-    setPredictedAnswerCheck( true );
-    console.log( predictedAnswer, predictedAnswerCheck );
+    setPredictedAnswer( val ); // value of the slider === the user's answer
+    setPredictedAnswerCheck( true ); // now check the answer
   };
 
+
+  const getDefaultValue = ( value ) =>
+  { 
+    return value;
+   }
+
+  const resetPredictedAnswer = (  ) =>
+  {
+    setPredictedAnswer( 0 );
+  };
 
   // Show Result
   const gameOver = () =>
   {
+    setPredictedAnswer( 0 ); // resets slider value
+    setValue( 0 );
+    getSliderValue(0);
+    setSliderDefault( 1 );
     setShowResult( true );
     setShowStart( false );
     setShowQuiz( false );
+
+
   };
 
   // Start the quiz again
@@ -129,6 +184,7 @@ export default function App ()
     setShowResult( false );
     setShowQuiz( true );
     setAnswerText( '' );
+    setAnswerAssessment( '' );
     setPredictedAnswer( 0 );
     setPredictedAnswerCheck( false );
     setShowAnswer( false );
@@ -139,14 +195,14 @@ export default function App ()
     setSliderMin( 1 );
     setSliderMax( 100 );
     setSliderDefault( 1 );
+    setValue(0);
+    getSliderValue(0);
   };
 
-
-
-
   return (
-    <div className="App">
-      {/* Welcome Page */ }
+
+      <Layout>
+
 
       <Start
         startQuiz={ startQuiz }
@@ -158,17 +214,28 @@ export default function App ()
         questions={ questions }
         checkAnswer={ checkAnswer }
         predictedAnswer={ predictedAnswer }
+        answerAssessment={ answerAssessment }
         answerText={ answerText }
         showAnswerText={ showAnswerText }
         showAnswer={ showAnswer }
         questionIndex={ questionIndex }
         nextQuestion={ nextQuestion }
-        setSlider={ setSlider }
+        getSliderValue={ getSliderValue }
         showResult={ showResult }
         gameOver={ gameOver }
         sliderMin={ sliderMin }
         sliderMax={ sliderMax }
         sliderDefault={ sliderDefault }
+        resetPredictedAnswer={resetPredictedAnswer}
+        handleValueChange={handleValueChange}
+        value={value}
+        getDefaultValue={getDefaultValue}
+        predictedAnswerCheck={predictedAnswerCheck}
+        valueSet={valueSet}
+        sliderLabel1={sliderLabel1}
+        sliderLabel={sliderLabel}
+        answer={answer}
+
 
 
       />
@@ -180,8 +247,9 @@ export default function App ()
         marks={ marks }
         startAgain={ startAgain } />
 
+{/* </div> */}
+</Layout>
 
-    </div>
 
   );
 }
